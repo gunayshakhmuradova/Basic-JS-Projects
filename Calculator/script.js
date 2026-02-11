84ec3af31572911b10560d33cbc9b8a6034fddc6
Gunay Shakhmuradova <126316477+gunayshakhmuradova@users.noreply.github.com>
2026-02-11 19:53:32 +0400

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,84 @@
+const display = document.querySelector('.calculator-input');
+const keys = document.querySelector('.calculator-keys');
+
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay() {
+    display.value = displayValue;
+}
+
+keys.addEventListener('click', function(e){
+    const element = e.target;
+    const value = element.value;
+
+    if(!element.matches('button')) return;
+
+    switch(value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear(); 
+            break; 
+        default:
+            inputNumber(element.value);        
+    }
+updateDisplay();
+});
+ 
+function inputNumber(num) {
+    if(waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+       displayValue = displayValue === '0'? num: displayValue + num;
+}
+};
+
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+
+    if(operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null){
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(10))}`;
+
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+};
+
+function calculate(first, second, operator) {
+    if(operator === '+') {
+        return first + second;
+    } else if (operator === '-') {
+        return first - second;
+    } else if (operator === '*') {
+        return first * second;
+    } else if (operator === '/') {
+        return first / second;
+    }
+
+    return second;
+};
